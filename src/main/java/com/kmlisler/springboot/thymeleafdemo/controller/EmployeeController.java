612218579424aa99bbffeb9ce441e516c3fbb05a
kmlisler <82888052+kmlisler@users.noreply.github.com>
2022-10-08 15:53:50 +0300

    process data for save employee, get datas orderby firstname

--- src/main/java/com/kmlisler/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/kmlisler/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.kmlisler.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -35,5 +37,11 @@ public class EmployeeController {
 
         return "employees/new-employee-form";
 
+    }
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("newEmployee") Employee theEmployee){
+        employeeService.save(theEmployee);
+
+        return "redirect:/employees/list";
     }
 }
--- src/main/java/com/kmlisler/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ src/main/java/com/kmlisler/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -3,7 +3,12 @@ package com.kmlisler.springboot.thymeleafdemo.dao;
 import com.kmlisler.springboot.thymeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
     // we get this crud methods with free !!
+
+    // get datas order by firstname
+    public List<Employee> findAllByOrderByFirstNameAsc();
 }
